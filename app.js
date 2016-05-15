d9987de48f476e8af3c9cc2b0e57e90ec5f4d8f7
Axel Bellec <axel.bellec@outlook.fr>
2016-05-16 01:13:13 +0200

    Handle environment variables

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
+var dotenv = require('dotenv').config();
+
+var access_token = process.env.FB_ACCESS_TOKEN;
+var verify_token = process.env.FB_VERIFY_TOKEN;
+
 
 // Parse all json contents
 app.use(bodyParser.json())
@@ -28,13 +33,20 @@ app.post('/webhook/', function (req, res) {
     if (event.message && event.message.text) {
       text = event.message.text;
       console.log(text);
+      if (text === 'Toulouse') {
+          getCityLastGeoJSON(text);
+          continue;
+      }
+
+      if (text === 'Website') {
+          sendGenericMessage(sender);
+          continue;
+      }
       sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
     }
   }
   res.sendStatus(200);
 });
-
-var token = "EAAPwJrok5ZBYBAIQdZBtZBFclZAw88ZAyMfs9NbR9681M7Guh37UYZCoLtDuxOD1cSZCL8NhvqclLeW79aZAu4UZAEEIgnDuZCk32qZAD53AhFtkSflRXOV6NVdDpau0ZA5FYXZC4yvLiku1TaZCkCSieua5nQBZC0aXuVwzZCWQ0ArvUA18AwZDZD";
 
 // Send message
 function sendTextMessage(sender, text) {
@@ -43,7 +55,7 @@ function sendTextMessage(sender, text) {
   }
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
+    qs: {access_token:access_token},
     method: 'POST',
     json: {
       recipient: {id:sender},
@@ -56,6 +68,57 @@ function sendTextMessage(sender, text) {
       console.log('Error: ', response.body.error);
     }
   });
+}
+
+// Send a message with a Template
+function sendGenericMessage(sender) {
+  messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "OpenBikes website",
+          "subtitle": "Not Just Biking",
+          "image_url": "http://openbikes.co/static/img/OpenBikes.png",
+          "buttons": [{
+            "type": "web_url",
+            "url": "http://openbikes.co",
+            "title": "Web url"
+          }],
+        }]
+      }
+    }
+  };
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:access_token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+// Make a call to OpenBikes api
+function getCityLastGeoJSON(city) {
+    var url = "http://openbikes.co/api/geojson/" + city
+
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body)
+        }
+    })
 }
 
 app.listen(process.env.PORT || 3000)
