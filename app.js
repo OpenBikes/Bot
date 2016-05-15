d025f872f71c8d408f75b395a5e41c8cd7638648
Axel Bellec <axel.bellec@outlook.fr>
2016-05-16 00:14:03 +0200

    - bot is now able to receive and send messages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var request = require('request');
 
 // Parse all json contents
 app.use(bodyParser.json())
@@ -27,9 +28,34 @@ app.post('/webhook/', function (req, res) {
     if (event.message && event.message.text) {
       text = event.message.text;
       console.log(text);
+      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
     }
   }
   res.sendStatus(200);
 });
+
+var token = "EAAPwJrok5ZBYBAIQdZBtZBFclZAw88ZAyMfs9NbR9681M7Guh37UYZCoLtDuxOD1cSZCL8NhvqclLeW79aZAu4UZAEEIgnDuZCk32qZAD53AhFtkSflRXOV6NVdDpau0ZA5FYXZC4yvLiku1TaZCkCSieua5nQBZC0aXuVwzZCWQ0ArvUA18AwZDZD";
+
+// Send message
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
 app.listen(process.env.PORT || 3000)
