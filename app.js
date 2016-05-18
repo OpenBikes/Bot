1c2efe956043fd15cc3c06333283653947c5b1bd
Axel Bellec <axel.bellec@outlook.fr>
2016-05-18 21:12:37 +0200

    - receive postback events and return payload content

--- app.js
+++ app.js
@@ -11,168 +11,229 @@ var verify_token = process.env.FB_VERIFY_TOKEN;
 // Parse all json contents
 app.use(bodyParser.json())
 
+// Listen to config port or to localhost:3000
+app.listen(process.env.PORT || 3000)
+
 // Test connection over modulus
 app.get('/ping', function(req, res) {
     res.send('pong')
 });
 
 // Verify crendentials
-app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === 'Wyv6tMhBrGZfQ9Ut') {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong validation token');
+app.get('/webhook', function(req, res) {
+    if (req.query['hub.verify_token'] === 'Wyv6tMhBrGZfQ9Ut') {
+        res.send(req.query['hub.challenge']);
+    }
+    res.send('Error, wrong validation token');
 })
 
 // Receive messaging events
-app.post('/webhook/', function (req, res) {
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      console.log(text);
+app.post('/webhook/', function(req, res) {
+    messaging_events = req.body.entry[0].messaging;
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+        if (event.message && event.message.text) {
+            text = event.message.text;
+            console.log(text);
 
-      if (text == 'Choose') {
-          sendPickACityMessage(sender);
-          continue;
-      }
+            if (text == 'Choose') {
+                sendMakeChoiceMessage(sender);
+                continue;
+            }
 
-      console.log(req.body.entry[0].messaging)
-      if (event.postback) {
-          text = event.postback;
-          console.log(text);
-          sendTextMessage(sender, "Postback received: "+text.substring(0, 200));
-          continue;
-      }
+            if (text === 'Countries') {
+                getCountries();
+                continue;
+            }
 
-      if (text === 'Toulouse') {
-          getCityLastGeoJSON(text);
-          continue;
-      }
+            if (text === 'Toulouse') {
+                getCityLastGeoJSON(text);
+                continue;
+            }
 
-      if (text === 'Website') {
-          sendGenericMessage(sender);
-          continue;
-      }
-      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+            if (text === 'Website') {
+                sendGenericMessage(sender);
+                continue;
+            }
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+        } else if (event.postback) {
+            text = event.postback.payload;
+            console.log(text);
+            if (text === 'pickbike') {
+                response = 'I notice you want to pick a bike'
+                pickbike(sender, response);
+            } else if (text === 'dropbike') {
+                response = 'I notice you want to drop a bike off'
+                dropbike(sender, response);
+            } else if (text === 'fulltrip') {
+                response = 'I notice you want to make a full trip'
+                fulltrip(sender, response);
+            }
+        }
     }
-  }
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 // Send message
 function sendTextMessage(sender, text) {
-  messageData = {
-    text:text
-  }
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:access_token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
+    messageData = {
+        text: text
     }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: access_token },
+        method: 'POST',
+        json: {
+            recipient: { id: sender },
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
 }
 
 // Send a message with a Template
 function sendGenericMessage(sender) {
-  messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [{
-          "title": "OpenBikes website",
-          "subtitle": "Not Just Biking",
-          "image_url": "http://openbikes.co/static/img/OpenBikes.png",
-          "buttons": [{
-            "type": "web_url",
-            "url": "http://openbikes.co",
-            "title": "Web url"
-          }],
-        }]
-      }
-    }
-  };
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:access_token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "OpenBikes website",
+                    "subtitle": "Not Just Biking",
+                    "image_url": "http://openbikes.co/static/img/OpenBikes.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "http://openbikes.co",
+                        "title": "Web url"
+                    }],
+                }]
+            }
+        }
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: access_token },
+        method: 'POST',
+        json: {
+            recipient: { id: sender },
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
 }
 
-// Make a call to OpenBikes API
+// Make a call to OpenBikes API to get countries
+function getCountries() {
+    var url = "http://openbikes.co/api/countries"
+
+    request({
+        url: url,
+        json: true
+    }, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            countries = body.data;
+            for (i = 0; i < countries.length; i++) {
+                country_name = countries[i].country;
+                console.log(country_name)
+            }
+        }
+    })
+}
+
+
+// Make a call to OpenBikes API for a city
 function getCityLastGeoJSON(city) {
     var url = "http://openbikes.co/api/geojson/" + city
 
     request({
         url: url,
         json: true
-    }, function (error, response, body) {
+    }, function(error, response, body) {
         if (!error && response.statusCode === 200) {
             stations = body.features;
             for (i = 0; i < stations.length; i++) {
-              station_name = stations[i].properties.name;
-              console.log(station_name)
+                station_name = stations[i].properties.name;
+                console.log(station_name)
             }
         }
     })
 }
 
-function sendPickACityMessage(sender) {
-  messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [{
-          "title": "OpenBikes",
-          "subtitle": "Choose a city",
-          "buttons": [{
-            "type": "postback",
-            "title": "Toulouse",
-            "payload": "Payload for first element in a generic bubble",
-          }],
-        }]
-      }
-    }
-  };
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:access_token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
+function sendMakeChoiceMessage(sender) {
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "OpenBikes",
+                    "subtitle": "Let me help you.",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Pick up a bike",
+                        "payload": "pickbike",
+                    }, {
+                        "type": "postback",
+                        "title": "Drop a bike off",
+                        "payload": "dropbike",
+                    }, {
+                        "type": "postback",
+                        "title": "Make a trip",
+                        "payload": "fulltrip",
+                    }],
+                }]
+            }
+        }
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: access_token },
+        method: 'POST',
+        json: {
+            recipient: { id: sender },
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
 }
 
 
-app.listen(process.env.PORT || 3000)
+// Pick a bike
+function pickbike(sender, text) {
+    sendTextMessage(sender, text);
+    askCity(sender);
+}
+
+// Drop a bike off
+function dropbike(sender, text) {
+    sendTextMessage(sender, text);
+    askCity(sender);
+}
+
+// Make a full trip
+function fulltrip(sender, text) {
+    sendTextMessage(sender, text);
+    askCity(sender);
+}
+
+function askCity(sender) {
+  response = 'Can you tell me in what city your are ?'
+  sendTextMessage(sender, response);
+}
